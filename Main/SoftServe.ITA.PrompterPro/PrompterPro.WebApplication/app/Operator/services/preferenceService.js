984823e4d2a5948469afc1ef877dbd8801379ff4
DanyloKiral <kiral.danylo@gmail.com>
2015-12-11 13:17:48 +0200

    no message

--- Main/SoftServe.ITA.PrompterPro/PrompterPro.WebApplication/app/Operator/services/preferenceService.js
+++ Main/SoftServe.ITA.PrompterPro/PrompterPro.WebApplication/app/Operator/services/preferenceService.js
@@ -1,10 +1,10 @@
 ﻿app.factory('preferenceService', [
-    'preferenceRepository', 'sectionServices', 'serverService', 'actorService', 
-function (preferenceRepository, sectionServices, serverService, actorService) {
+    'preferenceRepository', 'sectionServices', 'serverService', 'actorService', '$http',
+function (preferenceRepository, sectionServices, serverService, actorService, $http) {
         var currentScript;
         var currentActor;
         var currentSection;
-        var currentPreference;
+        var currentPreference; 
         return {
             setCurrentScript: function (script) {
                 currentScript = script;
@@ -26,18 +26,31 @@ function (preferenceRepository, sectionServices, serverService, actorService) {
  //               currentPreference = preferenceRepository(this,currentActor, currentScript.ScriptId);
             /*    var preference;
                  preference = preferenceRepository.get(actor.Id, script.ScriptId);*/
-                serverService.getPreference(actor.Id, script.ScriptId);
-                if (window.preference !== undefined && preference != null) {
+                //   serverService.getPreference(actor.Id, script.ScriptId);
+                $http.get("api/preference?id=" + actor.Id + "+" + script.ScriptId)
+                   .success(function (data) {
+                     //  console.log(data);
+                    //   var pref = data;
+                       $scope.speed = data.ReadingSpeed;
+                       $scope.textSize = data.FontSize;
+                       $scope.changeResolusion(data.ScreenWidth, data.ScreenHeight);
+                   })
+                    .error(function (data) {
+                        console.log('error');
+                        var pref = data;
+                    });
+             /*   if (window.preference !== undefined && preference != null) {
                     $scope.speed = preference.ReadingSpeed;
                     $scope.textSize = preference.FontSize;
-                    $scope.changeResolusion(preference.ScreenWidth, preference.ScreenHeight);
+                    $scope.changeResolusion(preference.ScreenWidth, preference.ScreenHeight);*/
                     //      return preference;
 
                     //      $scope.speed = currentPreference.ReadingSpeed;
                     //      $scope.textSize = currentPreference.FontSize;
                     //      $scope.changeResolusion(currentPreference.ScreenWidth, currentPreference.ScreenHeight);
-                }
+              //  }
             },
+
 
             exportSettings: function ($scope, width, height) {
                 var actor = window.currActor;
